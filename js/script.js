b2992b0d6ec9e30e60c2f1cad3daa3ac867259e8
Devheidson <devheidson@gmail.com>
2022-12-11 11:22:30 -0300

    Adicionado novos comentários ao projeto

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 // Realizar o cálculo do IMC ok
 // Mostrar o resultado do cálculo IMC --> ok
 
+// Construindo a função que realiza o cálculo do IMC e adiciona na página HTML o resultado
 function calcularIMC(){
     let peso = Number(document.querySelector("#peso").value);
     let altura = Number(document.querySelector("#altura").value);
@@ -11,6 +12,7 @@ function calcularIMC(){
     document.querySelector("#resultado").value = imc.toFixed(2);
 }
 
+// Selecionando a tag h2 da página HTML
 let h2 = document.querySelector("h2");
 h2.addEventListener("click", calcularIMC)
 
